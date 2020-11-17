56c37c8e78f036a0e662428a63f41737d42e70d9
Dani Alcala <daniamcode@gmail.com>
2020-11-17 01:28:53 +0100

    collapse accordion when clicked

--- src/components/SimpleAccordion.jsx
+++ src/components/SimpleAccordion.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import React from "react";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { Link } from "react-router-dom";
 
+export default function SimpleAccordion() {
+  const [expanded, setExpanded] = React.useState(false)
 
-export default function SimpleAccordion() {  
-
+  const handleChange = (event, expanded) =>{
+    setExpanded(expanded)
+  }
   return (
     <div>
-      <Accordion className="accordion">
+      <Accordion className="accordion" expanded={expanded} onChange={handleChange}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
@@ -19,12 +22,16 @@ export default function SimpleAccordion() {
           <p className="header__dropdown-categories">Resources</p>
         </AccordionSummary>
         <AccordionDetails>
-          <Link className="header__dropdown-subcategories" to="/about">
+          <Link
+            className="header__dropdown-subcategories"
+            to="/about"
+            onClick={handleChange}
+          >
             About
           </Link>
         </AccordionDetails>
         <AccordionDetails>
-          <Link className="header__dropdown-subcategories" to="/contact">
+          <Link className="header__dropdown-subcategories" to="/contact" onClick={handleChange}>
             Contact
           </Link>
         </AccordionDetails>
