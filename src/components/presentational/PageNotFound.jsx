f509864e81333262e744e4482814692cb28e7c90
Dani Alcala <daniamcode@gmail.com>
2020-11-26 20:56:43 +0100

    minor changes to pageNotFound style

--- src/components/presentational/PageNotFound.jsx
+++ src/components/presentational/PageNotFound.jsx
@@ -8,8 +8,8 @@ function PageNotFound(props) {
 		<main className="page-not-found__container">
             <section className="page-not-found__message">
 			<h1 className="page-not-found__title">Error - 404</h1>
-			<p className="page-not-found__text">Nothing to see here</p>
-			<Link className="page-not-found__link" to="/">Back to Dashboard</Link>
+			<p className="page-not-found__text">Ups, no content here</p>
+			<Link className="page-not-found__link" to="/">Back to Home</Link>
 			</section>
 		</main>
 	);
