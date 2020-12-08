2a86adfffd4ea06391ee8f891956c26d3ec243b5
Dani Alcala <daniamcode@gmail.com>
2020-12-08 13:39:54 +0100

    redo index test with hydrate not render

--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom";
 
-jest.mock("react-dom", () => ({ render: jest.fn() }));
+jest.mock("react-dom", () => ({ hydrate: jest.fn() }));
 
 describe("Application root", () => {
   it("should render", () => {
     require("./index.js");
-    expect(ReactDOM.render).toHaveBeenCalled();
+    expect(ReactDOM.hydrate).toHaveBeenCalled();
   });
 });
