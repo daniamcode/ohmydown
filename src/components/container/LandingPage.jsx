943350b45873cb3c90012b42ff5ab24e3656a68f
Dani Alcala <daniamcode@gmail.com>
2020-12-10 01:42:35 +0100

    change date prop name from countdown imported component

--- src/components/container/LandingPage.jsx
+++ src/components/container/LandingPage.jsx
@@ -83,7 +83,7 @@ const LandingPage = () => {
       </p>
       <h3 className="landingTable__title">Looking after 1000+ website's health:</h3>
       <p>(New real-time data in <span> </span>
-      <Countdown className="landingPage__countdown" timer={timer} daysInHours={true} overtime={true} />)</p>
+      <Countdown className="landingPage__countdown" date={timer} daysInHours={true} overtime={true} />)</p>
       <EnhancedTableLanding rawRows={rawRows}/>
       <br></br>
     </div>
