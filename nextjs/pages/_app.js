c4b29e076c6aef853299b8464114e73d265a88ea
Dani Alcala <daniamcode@gmail.com>
2021-01-20 17:00:27 +0100

    ssr for material-ui styles

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,42 +1,49 @@
 import React from "react";
-//import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 import "../styles/globals.css";
-//import theme from "../styles/theme";
-//import { ThemeProvider } from "@material-ui/core/styles";
-//import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../styles/theme";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import { Provider } from "react-redux";
 //import initStore, { initialState, preloadedState } from "../redux/configureStore";
-import finalStore, { initialState, preloadedState } from "../redux/configureStore";
+import finalStore, {
+  initialState,
+  preloadedState,
+} from "../redux/configureStore";
 // import withRedux from "next-redux-wrapper";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
 
 const store = finalStore(initialState);
 
+export const cache = createCache({ key: "css", prepend: true });
+
 function MyApp({ Component, pageProps }) {
-  // React.useEffect(() => {
-  //   // Remove the server-side injected CSS.
-  //   const jssStyles = document.querySelector("#jss-server-side");
-  //   if (jssStyles) {
-  //     jssStyles.parentElement.removeChild(jssStyles);
-  //   }
-  // }, []);
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
 
   return (
-    // <React.Fragment>
-    //     {/* <ThemeProvider theme={theme}> */}
-    //       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    //       {/* <CssBaseline /> */}
-          <Provider store={store}>
-            <Component {...pageProps} />
-          </Provider>
-    //     {/* </ThemeProvider> */}
-    // </React.Fragment>
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
-// MyApp.propTypes = {
-//   Component: PropTypes.elementType.isRequired,
-//   pageProps: PropTypes.object.isRequired,
-// };
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
 
 // MyApp.getServerSideProps = async ({ Component, ctx }) => {
 //   const pageProps = Component.getServerSideProps
