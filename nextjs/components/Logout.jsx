1d7bb87cf25884fd14f132985d766fc0d7482b02
Dani Alcala <daniamcode@gmail.com>
2021-01-25 19:33:01 +0100

    add Max-Age to cookies rather than expires

--- nextjs/components/Logout.jsx
+++ nextjs/components/Logout.jsx
@@ -38,7 +38,7 @@ const Logout = () => {
           document.cookie =
             "name=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
           dispatch(googleOAuth2());
-          // router.push('/')
+          router.push('/')
         }}
       />
     </>
